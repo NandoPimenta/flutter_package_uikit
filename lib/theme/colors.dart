4c05fe7294a4542ee2ecc154944f1425a4e4dde3
Fernando H. Pimenta <fernandopimentadev@gmail.com>
2024-02-01 10:03:05 -0300

    initial commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class CustomColor {
+  Color thirdColor = const  Color.fromRGBO(39, 51, 137, 1);
+  Color primaryColor = const  Color.fromRGBO(39, 51, 137, 1);
+  Color statusBarPrimaryColor = const  Color.fromRGBO(39, 51, 137, 1);
+  Color darkShadow = const Color.fromRGBO(0, 0, 0, 0.4);
+  Color backgroundLight = const Color.fromRGBO(247, 247, 247, 1);
+  Color error = const Color.fromRGBO(244, 67, 54, 1);
+  Color buttonTextColor = const Color.fromRGBO(255, 255, 255, 1);
+  Color secondary = const Color.fromRGBO(201, 150, 195, 1);
+  Color backgroundColor = const Color.fromRGBO(255, 255, 255, 1);
+  Color borderColor = const Color.fromRGBO(209, 209, 209, 1);
+  Color onPrimary = const Color.fromRGBO(13, 13, 13, 1);
+  Color transparent = const Color.fromRGBO(0, 0, 0, 0);
+  Color primaryDark = const Color.fromRGBO(0, 0, 0, 1);
+  Color disabledButton = const Color.fromRGBO(190, 190, 190, 1);
+  Color buttonAdd = const Color.fromRGBO(255, 207, 69, 1);
+  Color circleAvatar = const Color.fromRGBO(221, 196, 217, 1);
+  Color circleAvatarSecondary = const Color.fromRGBO(140, 109, 135, 1);
+  Color textGray = const Color.fromRGBO(131, 131, 131, 1);
+  
+}
